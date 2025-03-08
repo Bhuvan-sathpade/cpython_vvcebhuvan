386adf1800d8b66f164a069444cb216fcc15a4e7
Bhuvan-Sathpade <vvce22eee0021@vvce.ac.in>
2025-03-07 22:03:00 -0800

    1212

---
+++ progn.c
@@ -0,0 +1,2 @@
+git config --global user.name "Bhuvan-Sathpade"
+git config --global user.email "vvce22eee0021@vvce.ac.in"
---
+++ vvce2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0; i<3;i++){
+        for(int j=i ; i<3 ;j++ ){
+        printf("%d",j);
+        }
+     printf("\n");
+    }
+    return 0;
+}
